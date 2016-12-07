8c25c885bef8617b286d61ed7841d772e58835c0
Sylvain Perron <slvn.perron+github@gmail.com>
2016-12-07 12:18:29 -0500

    Save / restore config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import db from './db'
 
+const loadConfigFromFile = filePath => {
+  if (!fs.existsSync(filePath)) {
+    const config = {
+      manage_keywords: ['MANAGE_SUBSCRIPTIONS'],
+      manage_action : 'To unsubscribe, type: UNSUBSCRIBE_<CATEGORY>',
+      manage_type : 'text'
+    }
+    saveConfigToFile(config, filePath)
+  }
+
+  return JSON.parse(fs.readFileSync(filePath))
+}
+
+var saveConfigToFile = (config, filePath) => {
+  fs.writeFileSync(filePath, JSON.stringify(config))
+}
+
 module.exports = {
   init: function(bp) {
 
@@ -7,7 +24,8 @@ module.exports = {
 
   },
   ready: function(bp) {
-
+    const configFile = path.join(bp.projectLocation, bp.botfile.modulesConfigDir, 'botpress-subscription.json')
+    const config = loadConfigFromFile(configFile)
 
     bp.getRouter('botpress-subscriptions')
     .get('/subscriptions', (req, res) => {
